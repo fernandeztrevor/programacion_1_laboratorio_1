1b85b35e5df1d32c60cfb5f9e18cb9d4400860d0
fernandeztrevor <fernandeztrevor@outlook.com>
2018-03-30 19:06:19 -0300

    Add files via upload

--- FerreteConstruccion/main.c
+++ FerreteConstruccion/main.c
@@ -1,51 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-   float largo, ancho, radio, perimetro, superficie;
+   float largo;
+   float ancho;
+   float radio;
+   float perimetro;
+   float superficie;
    float cantcal=0;
    float cantcemento=0;
    float cantalambre=0;
-   char eleccion;
+   char eleccion=0;
+   float resultado;
+
+int main()
+{
 
    printf ("Bienvenido, seleccione a continuacion la operacion que desea realizar:\nA. Mostrar la cantidad de alambre a comprar para un terreno rectangular usando tres hilos de alambre\nB. Mostrar la cantidad de alambre para un terreno circular usando tres hilos de alambre.\nC. Mostrar cuantas bolsas de cemento y cal se necesitan para las medidas de un terreno rectangular.\n");
    scanf(" %c", &eleccion);
 
-    printf("\n\n %c", eleccion);
 
-    while (eleccion != 'a' || 'b' || 'c')
+while (eleccion != 'a' && eleccion !='b' && eleccion!='c')
 
    {
-       //printf(" leccion en while es: \n %c", eleccion);
 
        printf("Incorrecto, seleccione la operacion que desea realizar: ");
        scanf(" %c", &eleccion);
    }
+   switch (eleccion)
+        {
+        case 'a':
+        opcionA();
+        break;
 
+        case 'b':
+        opcionB();
+        break;
 
+        case 'c':
+        opcionC();
+        break;
+        }
 
-   if(eleccion=='a')
-   {
-       printf ("\nIngrese el largo del terreno en metros: ");
+    return 0;
+}
+
+int opcionA()
+{
+        printf ("\nIngrese el largo del terreno en metros: ");
         scanf("%f", &largo);
         printf ("\nIngrese el ancho del terreno en metros: ");
         scanf("%f", &ancho);
         perimetro=largo*2+ancho*2;
         cantalambre=perimetro*3;
         printf ("Usted necesita comprar %.2f metros de alambre", cantalambre);
-   }
-   if(eleccion=='b')
-   {
-       printf ("\nIngrese el radio del terreno en metros: ");
+
+        //return opcionA();
+};
+
+int opcionB()
+{
+        printf ("\nIngrese el radio del terreno en metros: ");
         scanf("%f", &radio);
         perimetro=2*radio*3,14;
         cantalambre=perimetro*3;
         printf ("Usted necesita comprar %.2f metros de alambre", cantalambre);
-   }
-   if(eleccion=='c')
-   {
-       printf ("\nIngrese el largo del terreno en metros: ");
+
+        //return opcionB();
+};
+
+int opcionC()
+{
+        printf ("\nIngrese el largo del terreno en metros: ");
         scanf("%f", &largo);
         printf ("\nIngrese el ancho del terreno en metros: ");
         scanf("%f", &ancho);
@@ -55,23 +80,4 @@ int main()
 
         printf ("Usted necesita comprar %.2f bolsas de cal",cantcal);
         printf (" y %.2f bolsas de cemento.",cantcemento);
-
-        //printf("%f", cantcemento);
-        /*
-
-
-
-        printf ("Usted necesita comprar %.2f bolsas de cal y %.2f bolsas de cemento."), cantcal, cantcemento/*(" y %.2f bolsas de cemento."), cantcemento;
-        */
-   }
-
-
-   /*printf ("\nIngrese el largo del terreno: %f");
-   scanf("%f", &largo);
-   printf ("\nIngrese el ancho del terreno: %f");
-   scanf("%f", &ancho);*/
-
-
-
-    return 0;
-}
+};
